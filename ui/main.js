12b4852e66aff659a05feed13ccae4aaedf60464
janakinrynn <31024064+janakinrynn@users.noreply.github.com>
2017-08-21 12:29:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,12 @@ console.log('Loaded!');
 //move the image madi
 
 var element = document.getElementById('image');
+var marginLeft =0;
+function moveRight()
+{   marginLfet = margniLeft +5;
+    element.style.marginLeft = marginLeft + 'px';
+    
+}
 element.onClick = function()   {    
           var interval = setInterval(moveRight, 100);
       };
