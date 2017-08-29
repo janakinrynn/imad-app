1bc262455e0bc5b1c20483c95c44518c095d982b
janakinrynn <31024064+janakinrynn@users.noreply.github.com>
2017-08-29 12:24:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,12 @@ console.log('Loaded!');
 var img = document.getElementById("image");
 var marginLeft = 0;
 function moveRight()
-{    marginLeft = marginLeft + 5;
-     img.style.marginLeft = marginLeft + 'px';
-    
-}
+    {    
+     marginLeft = marginLeft + 5;
+     img.style.marginLeft = marginLeft+'px';
+    }
 img.onClick = function() {  
-         img.style.marginLeft = '100px';
+         img.style.marginLeft='100px';
          // var interval = setInterval(moveRight, 50);
       };
     
