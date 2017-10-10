63b65f2218bec8aa977b1a29b9eb3e2226ac2ef3
janakinrynn <31024064+janakinrynn@users.noreply.github.com>
2017-10-10 14:23:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,22 +2,25 @@ console.log('Loaded!');
 
 //move the image madi
 
-//var img = document.getElementById("image");
-//var marginLeft = 0;
-//function moveRight()
- //   {    
-  //   marginLeft = marginLeft + 5;
-//     img.style.marginLeft = marginLeft+'px';
- //   }
-//img.onclick = function() {  
- //        img.style.marginLeft='100px';
+var img = document.getElementById("image");
+var marginLeft = 0;
+function moveRight()
+    {    
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft+'px';
+    }
+img.onclick = function() {  
+         img.style.marginLeft='100px';
          //var interval = setInterval(moveRight, 50);
-   //   };
+     };
       
 var counter = 0;
-var button = document.getElementById("counter");
+window.onload = function(){
+var btn = document.getElementById("counter");};
+
 console.log('button');
- button.onclick = function() {
+
+ btn.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById("count");
     span.innerHTML = counter.toString();
